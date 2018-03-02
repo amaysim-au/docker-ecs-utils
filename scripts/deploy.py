5944a3b92b3eada773f181b8984b59d903134c0a
Aaron Gorka <aaron.gorka@amaysim.com.au>
2018-03-02 16:04:23 +1100

    Add function to return next free priority

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,6 +4,8 @@ import sys, os, yaml, json, datetime
 # import boto3
 import subprocess
 from pprint import pprint
+import unittest
+from unittest.mock import patch
 
 if len(sys.argv) != 4 and len(sys.argv) != 3:
     exit('Usage: ./deploy.py <APP_NAME> <CLUSTER_NAME> [TASK_DEFINITION_IAM_ROLE]')
@@ -101,6 +103,38 @@ def update_target_group_health_check(alb_target_group, lb_health_check, lb_healt
 
     print("--> RUN: %s" % command)
     subprocess.check_output(command, shell=True)
+
+
+class GetPriorityTest(unittest.TestCase):
+    def test_1(self):
+        """Simple test to get next priority"""
+        rules = json.loads('[{"RuleArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:listener-rule/app/ECS-InternalTools-Dev/ba564ec55606a717/9c431593f1c78965/2cc6c973c4d32f55","Priority":"1","Conditions":[{"Field":"host-header","Values":["host1.asdf.com"]},{"Field":"path-pattern","Values":["/path2"]}],"Actions":[{"Type":"forward","TargetGroupArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:targetgroup/ecs-c-ALBDe-2TD7HNS9J92H/134e396d75ebd3a6"}],"IsDefault":false},{"RuleArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:listener-rule/app/ECS-InternalTools-Dev/ba564ec55606a717/9c431593f1c78965/f9994e3e3a55d6dd","Priority":"2","Conditions":[{"Field":"path-pattern","Values":["/path1"]}],"Actions":[{"Type":"forward","TargetGroupArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:targetgroup/ecs-c-ALBDe-2TD7HNS9J92H/134e396d75ebd3a6"}],"IsDefault":false},{"RuleArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:listener-rule/app/ECS-InternalTools-Dev/ba564ec55606a717/9c431593f1c78965/74a74e7da03f7ddb","Priority":"default","Conditions":[],"Actions":[{"Type":"forward","TargetGroupArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:targetgroup/ecs-c-ALBDe-2TD7HNS9J92H/134e396d75ebd3a6"}],"IsDefault":true}]')
+        priority = get_priority(rules)
+        self.assertEqual(priority, 3)
+
+    def test_2(self):
+        """Test with gap in list of priorities"""
+        rules = json.loads('[{"RuleArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:listener-rule/app/ECS-InternalTools-Dev/ba564ec55606a717/9c431593f1c78965/5cdf34d5cf48fabc","Priority":"1","Conditions":[{"Field":"path-pattern","Values":["/asdffdas"]}],"Actions":[{"Type":"forward","TargetGroupArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:targetgroup/ecs-c-ALBDe-2TD7HNS9J92H/134e396d75ebd3a6"}],"IsDefault":false},{"RuleArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:listener-rule/app/ECS-InternalTools-Dev/ba564ec55606a717/9c431593f1c78965/2cc6c973c4d32f55","Priority":"2","Conditions":[{"Field":"host-header","Values":["host1.asdf.com"]},{"Field":"path-pattern","Values":["/path2"]}],"Actions":[{"Type":"forward","TargetGroupArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:targetgroup/ecs-c-ALBDe-2TD7HNS9J92H/134e396d75ebd3a6"}],"IsDefault":false},{"RuleArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:listener-rule/app/ECS-InternalTools-Dev/ba564ec55606a717/9c431593f1c78965/284a6e35adc73d71","Priority":"5","Conditions":[{"Field":"path-pattern","Values":["/32452345"]}],"Actions":[{"Type":"forward","TargetGroupArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:targetgroup/ecs-c-ALBDe-2TD7HNS9J92H/134e396d75ebd3a6"}],"IsDefault":false},{"RuleArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:listener-rule/app/ECS-InternalTools-Dev/ba564ec55606a717/9c431593f1c78965/74a74e7da03f7ddb","Priority":"default","Conditions":[],"Actions":[{"Type":"forward","TargetGroupArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:targetgroup/ecs-c-ALBDe-2TD7HNS9J92H/134e396d75ebd3a6"}],"IsDefault":true}]')
+        priority = get_priority(rules)
+        self.assertEqual(priority, 3)
+
+    def test_3(self):
+        """Test with no rules except default"""
+        rules = json.loads('[{"RuleArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:listener-rule/app/ECS-InternalTools-Dev/ba564ec55606a717/9c431593f1c78965/74a74e7da03f7ddb","Priority":"default","Conditions":[],"Actions":[{"Type":"forward","TargetGroupArn":"arn:aws:elasticloadbalancing:ap-southeast-2:434027879415:targetgroup/ecs-c-ALBDe-2TD7HNS9J92H/134e396d75ebd3a6"}],"IsDefault":true}]')
+        priority = get_priority(rules)
+        self.assertEqual(priority, 1)
+
+
+def get_priority(rules):
+    """Returns the next available priority when given the response from aws elbv2 describe-rules"""
+    priorities = [rule['Priority'] for rule in rules]
+    i = 1
+    rule_priority = None
+    while not rule_priority:  # increment from 1 onwards until we find a priority that is unused
+        if not str(i) in priorities:
+            return i
+        else:
+            i = i + 1
 
 def update_listener_rule(alb_listener, alb_target_group, lb_path, lb_host):
     rules_json = subprocess.check_output("aws elbv2 describe-rules --listener-arn '"+alb_listener+"'", shell=True)
@@ -272,4 +306,5 @@ def main():
 
 
 if __name__ == "__main__":
+#    unittest.main(verbosity=2)
     main()
